af66674d9c22dc94bf4152e07de3e77192407973
rue-elwahishi <rubawahishi@gmail.com>
2019-10-16 17:42:39 +0100

    first commit

--- warmUp1.js
+++ warmUp1.js
@@ -1,4 +1,9 @@
 // 1-Create a third variable called z, assign x + y to it, and display it.
 var x = 5;
 var y = 10;
+var z = y + x
+console.log(z)
 // 2-calculate the area of a square using varaiables.
+function squareArea(x, y){
+ return x*y; 
+}
