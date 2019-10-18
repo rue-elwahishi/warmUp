df738a378783fee3d36ec6eb94fbf38b7de1a81f
rue-elwahishi <rubawahishi@gmail.com>
2019-10-18 09:55:42 +0100

    commit3

--- warmUp3.js
+++ warmUp3.js
@@ -2,7 +2,33 @@
 // -name of females in your class
 // -name of males in your class
 // -name of your class instructors 
+var females = ["Amera", "Fouz", "Hania", "Fatima", "Ons"]; 
+var males = ["Salem", "Hammam", "Muftah", "Belkheir", "Abdulsalam"];
+var instructors = ["Yousef", "Seif", "Matt", "hough"];
+
 
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
+function (array){
+	var elementInTheMiddle = [i];
+	for (var i in array){
+		if (array[i] === array[i] / array.length){
+			elementInTheMiddle.push(array[i]) 
+
+		}
+
+	}
+	return elementInTheMiddle;
+}
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+ function (array) {
+ 	var result = 1;
+ 	for (var i = 0; i < array.length; i + 2) {
+ 		result *= array[i] * 2; 
+
+
+
+
+ 	}
+ 	return result; 
+ }
