d97c3b30239bfb9ed47fc547e90fc15abe2c5e98
rue-elwahishi <rubawahishi@gmail.com>
2019-10-22 09:59:10 +0100

    newthang

--- warmup5.js
+++ warmup5.js
@@ -1,4 +1,61 @@
 // 1-create a data model to represent your classmates
+
+        var classmates = [ {
+        	name : "Hammam",
+        	gender : "male",
+        	height : "159cm",
+        	age : 24,
+        	hobby : "reading"}
+
+        	{name : "Muftah",
+        	gender : "male",
+        	height : "165cm", 
+        	age : 24
+        	hobby : "maths"}
+
+        	{name : "Salem",
+        	gender : "male",
+        	height : "165cm",
+        	age : 22,
+        	hobby : "gaming"}
+        ];   
+
+
+        function classmates(height, age, hobby) {
+
+
+        }
+         function displayFriend(mate) {
+         	return "this is my friend" + " " + classmates.name + " " + "he's" + " " + classmates.age + " " + "one of his hobbies is" + " " + classmatesl.hobby
+         }
+
+
+
+ 				
+
+			
+
+ 		function addFriend(mate) { 
+
+			for (var i = 0; i < classmates.length; i++){
+                     classmates.push([mate]);
+ 		
+ 		}	}
+ 		  
+
+ 		  function nbOfMale(classmates){
+ 		  	var arr = [];
+ 		  	var total = 0;
+ 		  	for (var i = 0; i < classmates.length; i++){
+ 		  		if (classmates[i][key] === "male"){
+ 		  			total += classmates[key];
+
+ 		  		}
+ 		  	}
+ 		  	 return total;
+
+ 		  }
+ 		 	
 //     -think of different attributes of your classmates? what do all of them have ?
 //     -create a factory function.
 //     -create an array to hold the classmates that you created and what you created to it .  
@@ -7,3 +64,10 @@
 //     -calculate the number of male friends that your class have by writing a function called nbOfMale.
 //     -Write a function searchMates that, given a query and an array of Mates,
 //           searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+			function searchMates(query, array) {
+
+				for (var i = 0; i < array.length; i++) {
+					if (array[i][key.age] === )
+				}
+			}
