15b127ed9e90e04fbb7e1b28d6770a8bd02b52ea
rue-elwahishi <rubawahishi@gmail.com>
2019-10-23 09:43:21 +0100

    newwarmup

--- warmUp6.js
+++ warmUp6.js
@@ -2,11 +2,17 @@
  //starting from 0, e.g.:
  mult(3); // => 6
  mult(4); // => 24
- function mult(n) {
- // your code is here
+  function mult(n) {
+ 
+ if (n === 0){
+ 	return 1;
+
+ }
+
+ return n * mult(n-1);
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
-// eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+// eg= 1 2 2 3 3 4 4 5 5 6 6 ... 
