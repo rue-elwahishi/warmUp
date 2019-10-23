4837ef13ae9fc5e10f15fa90c97e9f38d7a9b416
rue-elwahishi <rubawahishi@gmail.com>
2019-10-23 17:55:18 +0100

    edited

--- warmUp6.js
+++ warmUp6.js
@@ -15,4 +15,23 @@
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
-// eg= 1 2 2 3 3 4 4 5 5 6 6 ... 
+// eg= 1 2 2 3 3 4 4 5 5 6 6 ...  
+ function numbers(n) {
+	  var result = 0;
+      var i = 0;
+     
+      while ( i < n ){
+
+      	i++
+      
+       result = result + " " + i + " " + i ;
+
+      	
+
+      }
+
+      return result;
+
+	
+
+}
