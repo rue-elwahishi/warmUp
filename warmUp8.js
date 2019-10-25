5de68047c3b6ba2606b8b444ff951aa872ce089f
rue-elwahishi <rubawahishi@gmail.com>
2019-10-25 09:59:39 +0100

    hey

--- warmUp8.js
+++ warmUp8.js
@@ -2,3 +2,20 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+function isSubset ( array1, array2) {
+ 
+
+    for(var i = 0; i < array1.length; i++ ) {
+ 		for (var j = 0; j < array2.length; j++){
+              array1.slice(1)
+            if (array2[j] === array1[i]) {
+
+ 				return true
+ 			}
+ 			else  {
+ 				return false
+ 			}
+ 		}
+ 	}
+ }
+
