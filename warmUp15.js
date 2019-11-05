9d0c436a696cc3d8c320fa5b9b59cbfca36d04e5
rue-elwahishi <rubawahishi@gmail.com>
2019-11-05 09:54:34 +0100

    warmUp15

--- warmUp15.js
+++ warmUp15.js
@@ -1,6 +1,22 @@
 // write a function that takes a string as an argument 
 // and returns the string reversed word by word 
 // Example : reverseStr("hi there how is the weather today") ==>> "today weather the is how there hi"
+
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(string){
+	var str = ' '
+	var arr = [];
+	arr = string.split(" ")
+	for ( var i = arr.length -1; i >= 0; i--){
+           
+           str = arr[i] 
+          
+
+
+	}
+
+  return str;
+}
